f2a161f0df6a614d72f0efd0a7adb90951647ea0
lecar23 <leidy@example.com>
2025-03-01 15:49:49 -0500

    Actualización en la ruta de director

--- models/Director.js
+++ models/Director.js
@@ -3,8 +3,8 @@ const { Schema,model } = require ('mongoose');
 const DirectorSchema = Schema ({
     nombre:{ type: String ,required : true },
     estado:{ type: String ,required : true , enum: ['activo', 'inactivo'] }, 
-    fechacreacion:{ type: Date ,required : true },
-    fechaactualizacion:{ type: Date ,required : true },
+    fechacreacion: { type: Date, required: true },
+    fechaactualizacion: { type: Date, required: true },
   
 });
 module.exports = model('Director', DirectorSchema);
